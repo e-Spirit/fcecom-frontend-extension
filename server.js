0f216ab38473c3f5818f71eb8675d349454b3320
Team E-Commerce <team-e-commerce@e-spirit.com>
2022-12-02 13:26:09 +0000

    Updating to v1.1.0

---
+++ server.js
@@ -0,0 +1,11 @@
+﻿require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const { PORT } = process.env;
+const app = express();
+
+app.use(cors());
+app.use('/', express.static('src/firstspirit-preview'));
+
+app.listen(PORT ?? 3000, () => console.log(`Server started on PORT ${PORT ?? 3000}`));
